5baff5f66bf14b9ab264d717ac3fefa7024ef94b
Danylo Diachenko <danylodiachenko@gmail.com>
2024-01-16 18:30:25 +0200

    hw2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<template><StudentsList /></template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StudentsList from "./components/Students.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    StudentsList,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+ul {
+  list-style: none;
 }
 </style>
---
+++ src/components/Students.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cards">
+    <ul v-for="student in studentsList" :key="student.name">
+      <li>
+        <div>
+          <p>Name:</p>
+          <p>{{ student.name }}</p>
+        </div>
+        <div>
+          <p>Age:</p>
+          <p>{{ student.age }}</p>
+        </div>
+        <div>
+          <p>Height:</p>
+          <p>{{ student.height }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "StudentsList",
+  setup() {
+    const studentsList = [
+      { name: "John", age: 20, height: 175 },
+      { name: "Jane", age: 19, height: 165 },
+      { name: "Bob", age: 21, height: 180 },
+      { name: "Alice", age: 22, height: 170 },
+      { name: "David", age: 23, height: 185 },
+      { name: "Emily", age: 20, height: 160 },
+      { name: "Frank", age: 22, height: 178 },
+      { name: "Grace", age: 21, height: 172 },
+    ];
+
+    return {
+      studentsList,
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
